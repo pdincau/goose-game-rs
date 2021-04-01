367c8281b42665b6c8266bddf4a88b7698963dd7
Paolo D'Incau <paolo.dincau@gmail.com>
2021-04-01 22:43:03 +0200

    add simple runner

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,16 @@
+use crate::domain::game::{Game, GameError, Player};
+
 mod domain;
 
 fn main() {
-    println!("Hello, world!");
+    let mut game = Game::new();
+
+    if game.add(Player::new("Pietro")).is_ok() {
+        let player_names: Vec<String> = game.players().iter().map(|p| p.name().to_string()).collect();
+        println!("Players: {}", player_names.join(", "));
+    };
+    if game.add(Player::new("Paolo")).is_ok() {
+        let player_names: Vec<String> = game.players().iter().map(|p| p.name().to_string()).collect();
+        println!("Players: {}", player_names.join(", "));
+    }
 }
